a463547845c91bea38ffbc327bc139282212ed25
rowjay007 <rowlives@gmail.com>
2021-05-15 19:50:17 +0100

    wrapping up with Coin components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,47 @@
-import "./App.css";
-
-// https://api.binance.com/api/v3/ticker/24hr
+import axios from "axios";
+import { useEffect, useState } from "react";
+import Coin from "./components/Coin";
 
 function App() {
+  const [coins, setCoins] = useState([]);
+  // const [search, setSearch] = useState([]);
+  // const proxy = "https //cors-anywhere.herokuapp.com";
+  useEffect(() => {
+    axios
+      .get("https://api.binance.com/api/v3/ticker/24hr")
+      .then((res) => {
+        setCoins(res.data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  // const handleChange = (e) => {
+  //   setSearch(e.target.value);
+  // };
+
+  const handleFiltered = coins.filter((coin) => coin);
   return (
-    <div className="App">
-      <h2>Hello World</h2>
+    <div className="coin-app">
+      <div className="coin-search">
+        <h1 className="coin-text">Search a currency</h1>
+        <form>
+          <input type="text" placeholder="Search" className="coin input" />
+        </form>
+      </div>
+      {handleFiltered.map((coin) => {
+        return (
+          <Coin
+            key={coin.lastId}
+            askPrice={coin.askPrice}
+            prevClosePrice={coin.prevClosePrice}
+            priceChangePercent={coin.priceChangePercent}
+            symbol={coin.symbol}
+            volume={coin.volume}
+            price={coin.priceChange}
+            marketCap={coin.quoteVolume}
+          />
+        );
+      })}
     </div>
   );
 }
---
+++ src/components/Coin.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+const Coin = ({
+  priceChange,
+  priceChangePercent,
+  prevClosePrice,
+  symbol,
+  price,
+  volume,
+  marketCap,
+}) => {
+  return (
+    <div className="coin-container">
+      <div className="coin-row">
+        <div className="coin">
+          <p className="coin-symbol">{symbol}</p>
+          <h1>{priceChange}</h1>
+          <h1>{prevClosePrice}</h1>
+        </div>
+        <div className="coin-data">
+          <p className="coin-price">${price}</p>
+          <p className="coin-volume">${volume.toLocaleString()}</p>
+          {/* {priceChangePercent < 0 ? (
+            <p className="coin-percent red">{priceChangePercent.toFixed(2)}%</p>
+          ) : (
+            <p className="coin-percent green">{priceChangePercent.toFixed(2)}%</p>
+          )} */}
+          <p>%{priceChangePercent}</p>
+          <p className="coin-marketcap">
+            Mkt Cap: ${marketCap.toLocaleString()}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Coin;
